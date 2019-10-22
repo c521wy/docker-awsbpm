11794586f5338ebe0758289992ab031776ee542a
蔡伟强 <caiweiqiang@vip.qq.com>
2019-10-22 22:51:36 +0800

    修复数据库自启BUG

--- supervisor/supervisord-startup.sh
+++ supervisor/supervisord-startup.sh
@@ -2,7 +2,7 @@
 
 (
     sleep 5;
-    [[ "$DB_AUTO_START" = "true" ]] && supervisorctl start db;
+    [[ "$DB_AUTO_START" = "true" ]] && [[ "$USE_EXTERNAL_DATABASE" = "false" ]] && supervisorctl start db;
     [[ "$APP_AUTO_START" = "true" ]] && supervisorctl start app;
     [[ "$WEB_AUTO_START" = "true" ]] && supervisorctl start web;
 ) &
